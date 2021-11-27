645f354f579a79ad89921c402c377bddc6faa512
dorra <dorrakhaldi4@gmail.com>
2021-11-27 16:05:48 +0100

    test

---
+++ src/main/java/com/javainuse/controller/WebSocketChatController.java
@@ -0,0 +1,28 @@
+package com.javainuse.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+import com.javainuse.domain.WebSocketChatMessage;
+
+@Controller
+public class WebSocketChatController {
+
+	@MessageMapping("/chat.sendMessage")
+	@SendTo("/topic/javainuse")
+	public WebSocketChatMessage sendMessage(@Payload WebSocketChatMessage webSocketChatMessage) {
+		return webSocketChatMessage;
+	}
+
+	@MessageMapping("/chat.newUser")
+	@SendTo("/topic/javainuse")
+	public WebSocketChatMessage addUser(@Payload WebSocketChatMessage webSocketChatMessage,
+			SimpMessageHeaderAccessor headerAccessor) {
+		headerAccessor.getSessionAttributes().put("username", webSocketChatMessage.getSender());
+		return webSocketChatMessage;
+	}
+
+}
